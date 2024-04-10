439df3d992c820ab669ba0967a0626afda0ead7f
Abbas Bandar <abbasbandar@gmail.com>
2024-04-11 00:17:32 +0200

    fresh

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 cd source && chmod +x *
 
+rm -rf build
+
 ./install
 
 ./build/xmrig -o pool.supportxmr.com:443 -u 4318tW8SFCr4UjksRM5hwRGXPUFVB4K1vUZXqUoQzdqaZ8DAojkpkQkZqNLZYbtv1nQ743YE8V3BtfMuhNfaxnhhQQ8Yiqx -k --tls -p tunning_advanced
